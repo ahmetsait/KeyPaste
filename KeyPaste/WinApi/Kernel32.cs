2046cde1ac0a7ae164ebd48fe307c6206470222f
Ahmet Sait <git@ahmetsait.com>
2022-07-08 17:41:29 +0300

    Initial commit

---
+++ KeyPaste/WinApi/Kernel32.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace KeyPaste.WinApi
+{
+    class Kernel32
+    {
+        [DllImport("kernel32.dll")]
+        public static extern IntPtr OpenProcess(uint dwDesiredAccess, bool bInheritHandle, uint dwProcessId);
+
+        [DllImport("kernel32.dll")]
+        public static extern bool CloseHandle(IntPtr handle);
+    }
+}
---
+++ KeyPaste/WinApi/Psapi.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace KeyPaste.WinApi
+{
+    public static class Psapi
+    {
+        [DllImport("psapi.dll")]
+        public static extern uint GetModuleBaseName(IntPtr hWnd, IntPtr hModule, StringBuilder lpFileName, int nSize);
+
+        [DllImport("psapi.dll")]
+        public static extern uint GetModuleFileNameEx(IntPtr hWnd, IntPtr hModule, StringBuilder lpFileName, int nSize);
+    }
+}
